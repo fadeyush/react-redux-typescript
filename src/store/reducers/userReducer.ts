c11db0b385c982088b16897cffde761b37583980
fadeyush <a.fadeyush@gmail.com>
2024-02-09 17:45:16 +0300

    рефакторинг

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,38 +1,10 @@
-// взаимодействие со списком пользователей
-
-interface UsersState {
-    users: any[];
-    loading: boolean;
-    error: null | string;
-}
-
-enum UsersActionTypes {
-    FETCH_USERS = 'FETCH_USERS',
-    FETCH_USERS_SUCCESS= 'FETCH_USERS_SUCCESS',
-    FETCH_USERS_ERROR = 'FETCH_USERS_ERROR'
-}
-
-interface FetchUsersAction {
-    type: UsersActionTypes.FETCH_USERS,
-}
-
-interface FetchUsersSuccessAction {
-    type: UsersActionTypes.FETCH_USERS_SUCCESS,
-    payload: any[],
-}
-
-interface FetchUsersErrorAction {
-    type: UsersActionTypes.FETCH_USERS_ERROR,
-    payload: string,
-}
+import { UserAction, UsersActionTypes, UsersState } from "../../types/user";
 
 const inittialState: UsersState = {
     users: [],
     loading: false,
     error: null,
 }
-
-type UserAction = FetchUsersAction | FetchUsersErrorAction | FetchUsersSuccessAction
 
 export const userReducer = (state = inittialState, action: UserAction): UsersState => {
     switch (action.type) {
